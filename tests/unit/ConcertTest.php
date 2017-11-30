6d28680cc5671c5b13e33c4be438bdcf27a23ba3
jiaying <jiayingcai.jyc@gmail.com>
2017-12-01 00:42:51 +0800

    1.5 Refactoring for Speed

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -12,15 +12,36 @@ class  ConcertTest extends TestCase
     /** @test */
     function can_get_formatted_date()
     {
-        // Create a concert with a known date
-        $concert = factory(Concert::class)->create([
+        // Create a concert with a known date 
+        // factory->create : save on database
+        // factory->make : save on memory
+
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
        ]);
 
-        // Retrieve the formatted date
-        $date = $concert->formatted_date;
-
         // Verify the date is formatted as expected
-        $this->assertEquals('December 1, 2016', $date);
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
+
+    /** @test */
+    function can_get_formatted_start_time()
+    {
+        
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+       ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
